17e9752ff31567505298bb59a639d7afb060cdbf
T#3 @RR!V@L <esmail19980@gmail.com>
2020-04-24 22:45:46 +0800

    Update Ramadan-kareem.py

--- Ramadan-kareem.py
+++ Ramadan-kareem.py
@@ -1,40 +1,11 @@
-# An app for greeting people for ramadan
-import time
-import datetime
-
-when = input("when is Ramadan: ")
-
-
-if when == "tomorrow":
-   print(datetime.time)
-   print("From Esmail to everyone! ")
-   time.sleep(3)
-   print("   ____                           _              ")
-   print("  |  _ \ __ _ _ __ ___   __ _  __| | __ _ _ __   ")
-   print("  | |_) / _` | '_ ` _ \ / _` |/ _` |/ _` | '_ \  ")
-   print("  |  _ < (_| | | | | | | (_| | (_| | (_| | | | | ")
-   print("  |_| \_\__,_|_| |_| |_|\__,_|\__,_|\__,_|_| |_| ")
-   print("                                                 ")
-   print("   _                                             ")
-   print("  | | ____ _ _ __ ___  ___ _ __ ___              ")
-   print("  | |/ / _` | '__/ _ \/ _ \ '_ ` _ \             ")
-   print("  |   < (_| | | |  __/  __/ | | | | |            ")
-   print("  |_|\_\__,_|_|  \___|\___|_| |_| |_|            ")
-   time.sleep(2)
-   print("تصومو و تفطرو على خير وينعاد عليكم بالصحة و العافية 3>")
-else:
-    print("you should wait!")
-
-
 # an app for greeting people for ramadan
 import time
 import datetime
 
 when = input("when is Ramadan: ")
 
-
 if when == "tomorrow":
-   print(datetime.time)
+   print(datetime.time())
    print("From Esmail to everyone! ")
    time.sleep(3)
    print("   ____                           _              ")
@@ -52,5 +23,3 @@ if when == "tomorrow":
    print("تصومو و تفطرو على خير وينعاد عليكم بالصحة و العافية 3>")
 else:
     print("you should wait!")
-
-
